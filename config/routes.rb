59f5fad05d0fe15c888ae46ac52d822d9cb638ea
Justinette2175 <jugagnepain75@gmail.com>
2017-02-15 21:15:04 +0000

    implemented feature for admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,12 @@ Rails.application.routes.draw do
     delete :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: [:create, :show, :destroy]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
